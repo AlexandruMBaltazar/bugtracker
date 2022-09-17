a8815c859c8f6198cb98ec4d4964cd969258f6c6
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2022-09-17 14:43:46 +0100

    Jenkins perfom clean insall

--- common/jenkins/runner.groovy
+++ common/jenkins/runner.groovy
@@ -23,19 +23,20 @@ def runSharedStep(command) {
 
 def execute(step, services) {
     def availableServices = load "$env.WORKSPACE/common/jenkins/availableServices.groovy"
-    if (services == availableServices() && step == "Build") {
-        stage(step.name) {
-            buildAll(step.command)
-        }
-    } else {
-        stage(step.name) {
-            if (step.shared) {
-                runSharedStep(step.command)
-            } else {
-                parallel services.collectEntries {service -> [service, {runServiceStep(service, step.command)}]}
-            }
-        }
-    }
+    println(services == availableServices())
+//    if (services == availableServices() && step == "Build") {
+//        stage(step.name) {
+//            buildAll(step.command)
+//        }
+//    } else {
+//        stage(step.name) {
+//            if (step.shared) {
+//                runSharedStep(step.command)
+//            } else {
+//                parallel services.collectEntries {service -> [service, {runServiceStep(service, step.command)}]}
+//            }
+//        }
+//    }
 }
 
 return { services, pipeline -> pipeline.each {step -> execute(step, services) } }
